cecfeba47adb98877814aa526e5adb0eeaed3727
GahonaJordan <jgrobert62@gmail.com>
2023-08-22 17:16:26 -0500

    EXMAMEN

---
+++ grupo 2/CLASES/CONTINENTE.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * TODO Auto-generated comment.
+ */
+abstract class Continente {
+  public $nombre;
+  public $maxPaises;
+
+  public function __construct($nombre, $maxPaises) {
+      $this->nombre = $nombre;
+      $this->maxPaises = $maxPaises;
+  }
+}
